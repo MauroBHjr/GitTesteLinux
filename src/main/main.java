ead3c4501485ac2b2f2de665a1e6588771932779
MauroBHJr <abyss.angel@hotmail.com>
2022-07-24 18:39:32 -0300

    teste03

--- src/main/main.java
+++ src/main/main.java
@@ -4,8 +4,10 @@
  */
 package main;
 
+import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  *
@@ -17,6 +19,15 @@ public class main {
         System.out.println("Hello World");
         String message = "Hello World";
         JOptionPane.showMessageDialog(null,message,"Hello",JOptionPane.INFORMATION_MESSAGE);
+        
+        JLabel HelloWorldMessage = new JLabel("Hello World");
+        
+        JPanel painel01 = new JPanel();
+        
+        painel01.setLayout(new BorderLayout());
+        painel01.add(HelloWorldMessage,BorderLayout.CENTER);
+        
+        JOptionPane.showMessageDialog(null,painel01,"Hello02",JOptionPane.PLAIN_MESSAGE);
     }
     
 }
